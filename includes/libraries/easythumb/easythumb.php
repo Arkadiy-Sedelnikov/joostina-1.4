1115bca28a96413b576748e034acb3ce81c80aeb
Arkadiy-Sedelnikov <a.sedelnikov@gmail.com>
2011-11-30 18:46:05 +0600

    Новая библиотека для работы с изображениями.

---
+++ includes/libraries/easythumb/easythumb.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package Joostina
+ * @copyright Авторские права (C) 2008-2010 Joostina team. Все права защищены.
+ * @license Лицензия http://www.gnu.org/licenses/gpl-2.0.htm GNU/GPL, или help/license.php
+ * Joostina! - свободное программное обеспечение распространяемое по условиям лицензии GNU/GPL
+ * Для получения информации о используемых расширениях и замечаний об авторском праве, смотрите файл help/copyright.php.
+ * FeedCreator class v1.7.2
+ * originally (c) Kai Blankenhorn
+ * www.bitfolge.de
+ * kaib@bitfolge.de
+ * v1.3 work by Scott Reynen (scott@randomchaos.com) and Kai Blankenhorn
+ * v1.5 OPML support by Dirk Clemens
+ */
+
+defined('_VALID_MOS') or die();
+
+if(substr(PHP_VERSION, 0, 1) == 4)
+    include_once('inc/easyphpthumbnail_php4.class.php');
+else
+    include_once('inc/easyphpthumbnail_php5.class.php');
+?>
+
+
+
+
+
