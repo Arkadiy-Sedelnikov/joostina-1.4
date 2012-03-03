213fdaa4d39939f1db472af6d3fa66e4abf0c3cb
Arkadiy-Sedelnikov <a.sedelnikov@gmail.com>
2012-03-03 08:52:36 +0700

    Issue #56

--- components/com_boss/boss.php
+++ components/com_boss/boss.php
@@ -907,7 +907,7 @@ function show_content($contentid, $catid, $directory, $template_name) {
 		"LEFT JOIN #__boss_".$directory."_groups AS g ON fg.groupid = g.id \n".
 		"LEFT JOIN #__boss_".$directory."_fields AS f ON fg.fieldid = f.fieldid \n".
 		"WHERE g.published = 1 AND g.id IN ($groupids) AND f.published = 1 AND fg.type_tmpl = 'content' \n".
-		"ORDER BY f.ordering ASC, fg.ordering ASC ";
+		"ORDER BY fg.ordering, f.ordering";
         $database->setQuery( $query );
         $fieldsgrouptemp = $database->loadObjectList();
 
